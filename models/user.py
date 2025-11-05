993fb768932c72b6ab9d8dc9d277b86193ec3258
Jean Carlo <Jeancarlomachado14@gmail.com>
2025-11-05 17:36:43 -0300

    Desenvolvendo o user.py

---
+++ models/user.py
@@ -0,0 +1,10 @@
+from app import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    # id (int), username (text), password (text)
+    id = db.Column(db.Integer, primary_key=True) 
+    usarname = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    
