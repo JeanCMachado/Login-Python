153ce8517b1522489feb7f53f13f44c2b4fd83bd
Jean Carlo <Jeancarlomachado14@gmail.com>
2025-11-07 17:57:33 -0300

    criando parte de logout e criação de usuario

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -36,6 +36,25 @@ def login():
     
     return jsonify({"message": "Credenciais inválidas"}), 400
 
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado"})
+    
+    return jsonify({"message": "Dados invalidos"}), 400
 @app.route("/hello", methods=['GET'])
 def hello():
     return "hello world"
