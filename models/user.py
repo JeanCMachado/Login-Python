9218a893e8aa21ce2a14cbf0549cd71c6a90536a
Jean Carlo <Jeancarlomachado14@gmail.com>
2025-11-07 17:45:25 -0300

    logica de login

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     # id (int), username (text), password (text)
     id = db.Column(db.Integer, primary_key=True) 
-    usarname = db.Column(db.String(80), unique=True, nullable=False)
+    # map the ORM attribute `username` to the existing DB column named 'usarname'
+    username = db.Column('usarname', db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
 
